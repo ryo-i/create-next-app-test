f7873f008ae89e54db28284a187174ac734ee8e5
ryo-i <iidazz0109@gmail.com>
2021-04-24 17:31:28 +0900

    TypeScriptをインストール

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,8 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          たいぷすくりぷと、はじめました→{' '}
+          <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
